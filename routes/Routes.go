45a432e65cb12c76409ae61090de87f98c0e2d7d
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-12-02 17:16:22 +0100

    Add placeholder homepage '/'

--- routes/Routes.go
+++ routes/Routes.go
@@ -3,11 +3,17 @@ package Routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/nikolajjsj/go-jokes-api/api/controllers"
+	"net/http"
 )
 
 // SetupRouter function for initiating the Gin router
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
+
+	// Homepage of the api
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Welcome to go-jokes-api!\nPlease checkout \"/jokes\"")
+	})
 
 	// Router endpoints
 	router.GET("/jokes", Controllers.GetJokes)
